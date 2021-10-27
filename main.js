e90121e2d0c404b7be92d393bbc91c8471345ea6
Jerome Haynes <info.devrome@gmail.com>
2021-10-26 21:23:44 -0400

    ES6-exercises commit

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
 //Exercise #1
-// Define two arrays, evens and odds filled with even 
+// Define two arrays, evens and odds filled with even
 // and odd numbers
 // Using the spread operator, make a new
 // array with numbers that contains all of the evens and odds
+let even = [2, 4, 6, 8, 10];
+let odd = [1, 3, 5, 7, 9];
 
+let numbers = [...even, ...odd];
 
+console.log(numbers);
 
 //Exercise #2
-// Define an object favoriteFoods that has keys representing three of 
+// Define an object favoriteFoods that has keys representing three of
 // your favorite foods and the values should be the restaurant where
 // they are from
-// Make another object and do the same thing but ask your 
-// imaginary friend what their favorite foods are. 
-// Use the spread operator to build a final allFoods with the 
+// Make another object and do the same thing but ask your
+// imaginary friend what their favorite foods are.
+// Use the spread operator to build a final allFoods with the
 // items from the two objects you just built
 
+const favoriteFoods = {
+	burger: "Cheeburger Cheeburger",
+	pizaa: "Margherita Pizza",
+	chicken: "Popeyes Louisiana Kitchen",
+};
 
+const friendFavoriteFoods = {
+	steak: "LongHorn Steakhouse",
+	pasta: "Maestro Pasta",
+	sushi: "Sushi Nakazawa",
+};
+
+const allFoods = {
+	...favoriteFoods,
+	...friendFavoriteFoods,
+};
+
+console.log(allFoods);
 
 // Exercise #3
 // Define your favorite movie character in an object
@@ -29,4 +50,33 @@
 // 1) add a pet object to your person
 // 2) define three variables (age, breed, name) outside of the object
 // 3) add those variables to your pet
-// log the most important thing about it: the pet's name 
+// log the most important thing about it: the pet's name
+
+const favMovieCharacter = {
+	name: "Captain Jack Sparrow",
+	age: 60,
+	hometown: "Shipwreck Cove",
+};
+
+// const { name, age, hometown } = favMovieCharacter;
+// console.log(name, age, hometown);
+
+const { hometown, ...rest } = favMovieCharacter;
+console.log(rest);
+
+const pet = {
+	age: 20,
+	breed: "Scarlet macaw",
+	name: "Parrot",
+};
+
+const { name } = pet;
+
+console.log(name);
+
+let addPet = {
+	...favMovieCharacter,
+	...pet,
+};
+
+console.log(addPet);
